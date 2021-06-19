284c91dcd7719ff13c35ab2eccef0310d114e86c
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-06-18 19:27:35 -0700

    Update StonkBotDriver.py

--- StonkBotDriver.py
+++ StonkBotDriver.py
@@ -8,15 +8,24 @@ client = commands.Bot(command_prefix = '$')
 async def on_ready():
     print('Sample output- bot active')
 
+#DEV USE ONLY
+#=====================
+@client.command
+async def latency(ctx):
+    await ctx.send('{round(client.latency * 1000)}ms')
+#=====================
 
-@client.command 
+@client.command(aliases = ['cmdlist'])
 async def legend(ctx):
-    await ctx.send('Return daily movers: $DailyMovers, DailyMovers, DM')
+    await ctx.send('all commands prefixed by $ symbol\nReturn daily movers: DailyMovers, DailyMovers, DM')
 
 @client.command(aliases = ['DailyMover', 'DailyMovers'])
 async def DM(ctx):
-   await ctx.send('Test')
+   await ctx.send('Test output')
 
+@client.command
+async def GetTicker(ctx):
+    await ctx.send('test output')
     
 client.run('ODU0MDcyMzQ3MzU4NjU4NTcw.YMem2w.sL01rRwKibWfQ13u0tjepcoZt3s')
 
