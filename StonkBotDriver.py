2364f4bb96ade0b9f3d8fe735ac0a068e2a8391d
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-07-16 11:31:59 -0700

    Update StonkBotDriver.py

--- StonkBotDriver.py
+++ StonkBotDriver.py
@@ -6,7 +6,7 @@ client = commands.Bot(command_prefix = '$')
 
 @client.event
 async def on_ready():
-    print('Sample output- bot active')
+    print('bot active')
 
 #DEV USE ONLY
 #=====================
