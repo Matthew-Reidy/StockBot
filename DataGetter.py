71b3a34c6a824552ef84754d254a96268e9d799c
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-07-16 14:39:28 -0700

    Update DataGetter.py

--- DataGetter.py
+++ DataGetter.py
@@ -1,6 +1,7 @@
 import StockBot
 import requests
 import json
+
 url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete"
 
 headers = {
@@ -10,19 +11,22 @@ headers = {
 
 
 class dataget:
+    def __init__(self, symbol):
+        self.ticker= symbol
       
-    def getQuote(tick):
+        def get_quote(self):
+         querystring = {"q" : symbol ,"region":"US"}
+         response = requests.request("GET", url, headers=headers, params=querystring)
+         data= json.loads(response)
      
-     querystring = {"q" : tick ,"region":"US"}
-     response = requests.request("GET", url, headers=headers, params=querystring)
-     data= json.loads(response)
-     
-     for quote in data['result']:
-         print(quote['symbol', 'regularMarketPrice'])
+         for quote in data['result']:
+            print(quote['symbol', 'regularMarketPrice'])
          
 
-def getDM():
-  DMquerystring = {"region": "US"}
-  DMresponse = requests.request("GET", url, headers=headers, params= DMquerystring)
-  data=json.loads(DMresponse)
+         def get_DM(self):
+          DMquerystring = {"region": "US"}
+          DMresponse = requests.request("GET", url, headers=headers, params= DMquerystring)
+          data=json.loads(DMresponse)
 
+dg= dataget()
+dg.get_quote()
