ae22e94e5b9b3a82a610f0ab5baa2e3e3c35668f
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2022-08-13 16:18:58 -0700

    removed class usage in favor of functional style

--- DataGetter.py
+++ DataGetter.py
@@ -1,6 +1,7 @@
 import StockBot
 import requests
 import json
+import os 
 
 url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete"
 
@@ -10,23 +11,19 @@ headers = {
     }
 
 
-class dataget:
-    def __init__(self, symbol):
-        self.ticker= symbol
-      
-        def get_quote(self):
-         querystring = {"q" : symbol ,"region":"US"}
-         response = requests.request("GET", url, headers=headers, params=querystring)
-         data= json.loads(response)
+#gets quote of 
+def get_quote(self):
+    querystring = {"q" : symbol ,"region":"US"}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    data= json.loads(response)
      
-         for quote in data['result']:
-            print(quote['symbol', 'regularMarketPrice'])
+    for quote in data['result']:
+        print(quote['symbol', 'regularMarketPrice'])
          
+#retreives daily movers
+def get_DM(self):
+    DMquerystring = {"region": "US"}
+    DMresponse = requests.request("GET", url, headers=headers, params= DMquerystring)
+    data=json.loads(DMresponse)
 
-         def get_DM(self):
-          DMquerystring = {"region": "US"}
-          DMresponse = requests.request("GET", url, headers=headers, params= DMquerystring)
-          data=json.loads(DMresponse)
-
-dg= dataget()
-dg.get_quote()
+ 
