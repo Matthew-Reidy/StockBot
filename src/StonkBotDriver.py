225d1b857e9339e7080b5a67cdd82b8edcb2bfb7
Matthew Reidy <matthewreidy5@gmail.com>
2023-04-30 14:28:34 -0700

    working request module and data return

--- src/StonkBotDriver.py
+++ src/StonkBotDriver.py
@@ -1,39 +1,52 @@
-# import discord
+import discord
 from discord.ext import commands
-import DataGetter
-import os
-client = commands.Bot(command_prefix = '$')
+import requestModule
+client = commands.Bot(command_prefix='$')
+
 
 @client.event
 async def on_ready():
-    print('bot active')
-
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    
 #DEV USE ONLY
 #=====================
-@client.command()
-async def latency(ctx):
-    await ctx.send('{round(client.latency * 1000)}')
+# @client.command()
+# async def latency(ctx):
+#     await ctx.send(client.latency)
+#     client.run()
 #=====================
 
+@client.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+    
 @client.command(aliases = ['cmdlist'])
 async def legend(ctx):
-    await ctx.send('all commands prefixed by $ symbol\nReturn daily movers: DailyMovers, DailyMovers, DM')
+    await ctx.channel.send('all commands prefixed by $ symbol\nReturn daily movers: DailyMovers, DailyMovers, DM')
+   
 
 @client.command(aliases = ['DailyMover', 'DailyMovers'])
 async def DM(ctx):
-   await ctx.send(DataGetter.getDM)
+   dms = ""
+   payload = requestModule.getDMs()
+
+   await ctx.send( "test" )
+   
 
 #retrieves ticker info
-@client.command()
-async def GetTicker(ctx, msg):
-    tick=msg
-    DataGetter.GetQuote(tick)
+@client.command(aliases=['getQuote', 'getquote'])
+async def GetTickerQuote(ctx, msg):
+   
+    pass
+   
 
 @client.command(aliases = ['markethours'])
 async def MarketHours(ctx):
      await ctx.send('https://www.nyse.com/markets/hours-calendars')
+     
 
-
-#dummy discord token    
-client.run(os.environ['DISCORD_KEY'])
+client.run()
 
--- src/requestModule.py
+++ src/requestModule.py
@@ -1,29 +1,36 @@
-
-import requests
+import requests as request
 import json
 import os
 
-url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete"
+url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2"
 
 headers = {
-    'x-rapidapi-key': os.environ['RAPID_API_KEY'],
-    'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
+    'x-rapidapi-key': "",
+    'x-rapidapi-host': ""
     }
 
+def getDMs():
+    endpoint = '/get-movers'
+    result = request.get(url + endpoint,headers=headers)
 
-#gets quote of 
-def get_quote(self):
-    querystring = {"q" : symbol ,"region":"US"}
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    data= json.loads(response)
-     
-    for quote in data['result']:
-        print(quote['symbol', 'regularMarketPrice'])
-         
-#retreives daily movers
-def get_DM(self):
-    DMquerystring = {"region": "US"}
-    DMresponse = requests.request("GET", url, headers=headers, params= DMquerystring)
-    data=json.loads(DMresponse)
+    body = json.loads(result.text)
 
+    bodylist = body["finance"]["result"]
+  
+    dailymovers: list[str] = []
+    for elements in range(len(bodylist)):
+        dailymovers.append(bodylist[elements]["title"])
+        quotelist = bodylist[elements]["quotes"]
+        for i in range(len(quotelist)):
+            dailymovers.append(quotelist[i]["symbol"])
+    return dailymovers
+
+
+def getQuote(ticker):
+    endpoint = '/get-quotes'
+    result = request.get(url+endpoint,headers=headers)
+    body= json.loads()
+    
+        
+   
  
